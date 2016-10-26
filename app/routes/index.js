be6e14a449f686ad4d07118c2b34a940f70af34a
Ian Baum and Meredith Alcorn <meredith@>
2016-10-26 16:29:37 -0400

    mid update function

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,6 +13,10 @@ export default Ember.Route.extend({
       var newPost = this.store.createRecord('post', params);
       newPost.save();
       this.transitionTo('index');
+    },
+    destroyPost(post){
+      post.destroyRecord();
+      this.transitionTo('index');
     }
   }
 });
